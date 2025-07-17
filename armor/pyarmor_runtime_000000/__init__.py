322011c52a63b16a7a82a75d50c917e2f3fa1622
Yadh <yadhnikawakde@gmail.com>
2025-07-17 23:14:35 +0530

    clean pyarmor

---
+++ armor/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.1.7 (trial), 000000, 2025-07-17T22:55:25.226879
+from .pyarmor_runtime import __pyarmor__
--- dist/docpat/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.1.7 (trial), 000000, 2025-07-16T21:47:25.890043
-from .pyarmor_runtime import __pyarmor__
